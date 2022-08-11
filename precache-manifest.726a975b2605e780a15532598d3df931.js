17b7b8d41db4b8b49328b56fa3084324e6a45062
Pierre Tran <pierretranva@gmail.com>
2022-08-11 18:38:04 -0400

    Updates

---
+++ precache-manifest.726a975b2605e780a15532598d3df931.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "deb8375540bdc80549ec",
+    "url": "/static/css/main.821715d6.chunk.css"
+  },
+  {
+    "revision": "deb8375540bdc80549ec",
+    "url": "/static/js/main.deb83755.chunk.js"
+  },
+  {
+    "revision": "fb7d56ae6e5b9425c9bd",
+    "url": "/static/js/1.fb7d56ae.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6b9acf022f710a5f498b07cdbc22a38b",
+    "url": "/index.html"
+  }
+];
